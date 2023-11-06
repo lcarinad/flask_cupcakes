795e67546a1165f0a8f7c551907f2ca08032f89f
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-11-06 15:47:53 -0500

    routes for get and post requests

--- app.py
+++ app.py
@@ -9,3 +9,28 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "oh-so-secret"
 app.app_context().push()
 connect_db(app)
+
+@app.route('/api/cupcakes')
+def list_cupcakes():
+    all_cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes = all_cupcakes)
+
+@app.route('/api/cupcakes/<int:id>')
+def get_cupcake(id):
+    cupcake = Cupcake.query.get_or_404(id)
+
+    return jsonify(cupcake = cupcake.serialize())
+
+@app.route('/api/cupcakes', methods = ["POST"])
+def create_cupcake():
+    flavor = request.json.get("flavor")
+    size = request.json.get("size")
+    rating = request.json.get("rating")
+    image = request.json.get("image")
+    
+    new_cupcake = Cupcake(flavor=flavor, size = size, rating = rating, image = image)
+    
+    db.session.add(new_cupcake)
+    db.session.commit()
+    response_json=jsonify(cupcake=new_cupcake.serialize())
+    return (response_json, 201)
--- models.py
+++ models.py
@@ -14,4 +14,17 @@ class Cupcake(db.Model):
     flavor = db.Column(db.Text, nullable = False)
     size = db.Column(db.Text, nullable = False)
     rating = db.Column(db.Float, nullable = False)
-    image = db.Column(db.Text, nullable = False, default = "https://tinyurl.com/demo-cupcake")
+    image = db.Column(db.Text, default = "https://tinyurl.com/demo-cupcake")
+    
+    def serialize(self):
+        """Returns a dict representation of cupcakes for JSON conversion"""
+        return {
+            'id':self.id,
+            'flavor':self.flavor,
+            'size':self.size,
+            'rating':self.rating,
+            'image': self.image
+        }
+        
+    def __repr__(self):
+        return f"<Cupcake= {self.id}, Flavor={self.flavor}>"
