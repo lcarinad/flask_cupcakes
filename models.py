ccf5a8a0082e365d53afcf8b6cdfd941332c0006
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-11-06 14:56:53 -0500

    cupcake db and app.py

---
+++ models.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    """Models for Cupcake app."""
+    __tablename__="cupcakes"
+    
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    flavor = db.Column(db.Text, nullable = False)
+    size = db.Column(db.Text, nullable = False)
+    rating = db.Column(db.Float, nullable = False)
+    image = db.Column(db.Text, nullable = False, default = "https://tinyurl.com/demo-cupcake")
